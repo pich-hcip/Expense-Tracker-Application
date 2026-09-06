4ae8d221bb3b4def61e086d0a71be3b9870657c9
Theary <chch71774@gmail.com>
2026-09-06 09:25:58 +0700

    update main.dart before pulling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/login.dart';
+import 'screens/get_start.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -19,8 +19,8 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
 
-      // Start with Login Page
-      home: const LoginScreen(),
+      // Start with Get Started Page
+      home: const GetStartScreen(),
     );
   }
 }
